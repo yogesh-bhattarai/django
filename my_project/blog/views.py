cc64891b86f0fbc895394db75bc122146b2a36cf
Yogesh <yogesh.bhattarai633@gmail.com>
2025-05-05 17:51:38 +0545

    form created

--- my_project/blog/views.py
+++ my_project/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
-    return HttpResponse("Hello, Nischal")
+    return render(request, 'index.html')
 # Create your views here.
