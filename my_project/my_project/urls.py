bb8c3f2d7866cb3489d3a77f42d6227c8d03d803
Yogesh <yogesh.bhattarai633@gmail.com>
2025-05-06 09:42:20 +0545

    templates setup

--- my_project/my_project/urls.py
+++ my_project/my_project/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path,include
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("",include("blog.urls")),
+    path("",views.home,name= "home"),
+    path("contact/",views.contact,name="contact"),
 ]
---
+++ my_project/my_project/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request,"index.html")
+
+def contact(request):
+    return HttpResponse("this is contact page")
