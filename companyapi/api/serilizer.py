55d06e18f2a4d4ea7a96fd52b2a845ddf0dc7a99
Yogesh <yogesh.bhattarai633@gmail.com>
2025-05-13 14:32:19 +0545

    fix bug

--- companyapi/api/serilizer.py
+++ companyapi/api/serilizer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Company
 
 class CompanySerilizer(serializers.HyperlinkedModelSerializer):
+    company_id= serializers.ReadOnlyField()
     class Meta:
         model= Company
         fields= '__all__'
