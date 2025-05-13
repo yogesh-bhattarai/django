01dfed5e4b9b2954c7c5eb7ef2361cfd5d7c5c46
Yogesh <yogesh.bhattarai633@gmail.com>
2025-05-13 14:42:14 +0545

    created employee class

--- companyapi/api/models.py
+++ companyapi/api/models.py
@@ -10,3 +10,12 @@ class Company(models.Model):
     types= models.CharField(max_length=100, choices=(('IT','IT'),('Non IT','Non IT'),('Mobile Phones','Mobile Phones')))
     active= models.BooleanField(default=True)
 
+class Employee(models.Model):
+    employee_id= models.AutoField(primary_key=True)
+    name= models.CharField(max_length=100)
+    email= models.EmailField(max_length=100)
+    address= models.CharField(max_length=100)
+    about= models.TextField()
+    position= models.CharField(max_length=100, choices=(('Manager','Manager'),('Software Developer','Software Developer'),('Project Leader','Project Leader')))
+    company= models.ForeignKey(Company,on_delete=models.CASCADE)
+
